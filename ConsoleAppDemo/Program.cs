e17185b6fdcc054213d8740ba3b3f687fd80be5b
Libuser <Libuser@LIBUSER15>
2023-10-12 09:02:05 +0200

    Add project files.

---
+++ ConsoleAppDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿using static System.Console;
+
+// See https://aka.ms/new-console-template for more information
+string sayHelloToTheWorld = "Hello, World!";
+
+WriteLine(sayHelloToTheWorld);
+
+
+
+
